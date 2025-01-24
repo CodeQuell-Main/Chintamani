ac3765d329e2444f0af6d7e2eaae0422092696e4
Krishna Kadukar <krishnakadukar004@gmail.com>
2025-01-24 14:40:23 +0530

    products

--- Backend/data/Konkan.js
+++ Backend/data/Konkan.js
@@ -4,8 +4,7 @@ const Konkan = [
         productImage:'https://i.postimg.cc/8zdbTTt6/konkan-1.png',
         productName:'आंबापोळी',
         productPrice:'550',
-        productDetail:`   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail:`आंबा कोळी थोडक्यात काही क्षणातच तोंडात टाकल्यावर आंब्याची चव दाखवणारी पोळी म्हणजे आंबा पोळी या आंबा पोळी बनवण्याच्या प्रक्रियेमध्ये प्रथम हा मँगो पल्प ओवन मध्ये टेंपरेचर वर सुकवला जातो व त्यापासून आंबा पोळी बनवली जाते खास करून छोट्या मुलांना ही आंबापोळी या खूप आवडतते. हे उत्पादन आमच्या येथे शंभर ग्रॅम मध्ये उपलब्ध आहे.`,
         Category:'konkanproduct'
     },
 
@@ -14,8 +13,7 @@ const Konkan = [
         productImage:'https://i.postimg.cc/t4G50stb/konkan-2.png',
         productName:'फणसपोळी',
         productPrice:'500',
-        productDetail:`   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail:`फणस पोळी थोडक्यात काही क्षणात तोंडात टाकल्यावर फणसाच्या गऱ्याची चव चाकवणारी ट्रॉफी सारखी पोळी म्हणजेच फणस पोळी. सर्वप्रथम रसाळ फणसाचा पल्प हा ओऊवन मध्ये विशिष्ट प्रकारे विशिष्ट टेंपरेचर वर सुकवला जातो व त्यापासून फणस पोळी बनवली जाते खास करून लहान मुलांना ही फणस पोळी खूप आवडते. हे उत्पादन आमच्या येथे शंभर ग्रॅम मध्ये उपलब्ध आहे `,
         Category:'konkanproduct'
     },
 
@@ -24,8 +22,7 @@ const Konkan = [
         productImage:'https://i.postimg.cc/C15CCkYG/konkan-3.png',
         productName:'उकडे तांदूळ (वालय) (तुकडा )',
         productPrice:'90',
-        productDetail:`   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail: `उकडे तांदूळ हा पेजेसाठी वापरला जाणारा तांदूळ .कोकणात पेज हा लागतेच मधल्या वेळेत पटकन भूक भागवणारा पदार्थ. पटकन तरतरी आणणारा व उत्साह वाढवणारा पदार्थ म्हणजे पेज. पाणी जास्त घालून हे तांदूळ शिजवले जातात. शेतात काम करणाऱ्यांना तर याची पेज हे महत्त्वाचे खाद्य असते. मुख्यत्वे करून आजारपणात रुग्णांना आमच्या कोकणात उकड्या तांदळाची पेज हेच मुख्य अन्न म्हणून दिले जाते. आणि आमच्या जवळून विकले जाणारे उकडे तांदूळ हे पूर्णतः सेंद्रिय खतापासून बनवलेले असल्याने आरोग्यास अपाय होत नाही. आमच्याकडे हे फक्त व वालय या जातीचे पोहे उपलब्ध असतात. याच्यात दोन प्रकार असतात ,एक अख्खा तांदूळ व दुसरा दुसरा तांदूळ हे 500ग्रॅम व 1 किलो मध्ये उपलब्ध आहेत." `,
         Category:'konkanproduct'
     },
 
@@ -34,8 +31,7 @@ const Konkan = [
         productImage:'https://i.postimg.cc/nLHGzNPw/konkan-4.png',
         productName:'गावठी पोहे (वालय)',
         productPrice:'100',
-        productDetail:`   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail:`पोहे आमच्याकडे वालय या जातीच्या तांदळापासून बनवले जातात . हे पोहे जाड व तांबडे असतात. हे पोहे नुसत्या चहा मध्ये बुडवून खाल्ले तरी चालतात, त्याच्या बरोबर गूळ घालून गोड पोहे व तिखट पोहे पण बनवू शकता. हे 500 ग्रॅम तसेच एक किलो मध्ये उपलब्ध आहेत.`,
         Category:'konkanproduct'
     },
 
--- Backend/data/Syrup.js
+++ Backend/data/Syrup.js
@@ -4,8 +4,7 @@ const Syrup = [
         productImage: 'https://i.postimg.cc/GpCThYHL/syrup-Products-1.png',
         productName: 'कोकम सरबत',
         productPrice: '140',
-        productDetail: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail: `कोकम सरबत कोकम हे कोकणातील अत्यंत उपयोगी फळ आहे की ज्यात पाचक गुणधर्म आहेत. त्यापासून बनवले जाणारे सरबत हे उन्हाळ्यात आवडीने केले जाते कोणत्याही कृत्रिम कलर शिवाय तयार केलेले हे आमचे उत्पादन आपणासाठी एक लिटर व अर्धा लिटर मध्ये आणले आहे. साखर मिश्रित हेच सरबत लहान मुलांपासून मोठ्यांपर्यंत आवडीने सेवन केले जाते या सर्व त्याचे पाण्याबरोबर एकास चार वाटी असे प्रमाण आहे.`,
         Category: 'syrup'
     },
 
@@ -14,8 +13,7 @@ const Syrup = [
         productImage: 'https://i.postimg.cc/L8fZpv4Q/syrup-Products-2.png',
         productName: 'कोकम आगळ',
         productPrice: '120',
-        productDetail: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail: `कोकम आगळ या कोकम रसात केवळ नीट वापरले जाते. विशेषतः सोलकढीसाठी सर्वत्र याचा वापर केला जातो.`,
         Category: 'syrup'
     },
 
@@ -24,8 +22,7 @@ const Syrup = [
         productImage: 'https://i.postimg.cc/Y2nFfhyR/syrup-Products-3.png',
         productName: 'आवळा सरबत',
         productPrice: '120',
-        productDetail: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail: `आवळा सरबत साखरयुक्त आवळ्याचा रस जो पित्तशामक व पाचक औषधा गुणांमुळे आरोग्यास अतिशय उत्तम असा आहे. मुख्यत्वे करून उन्हाळ्याच्या दिवसात याचा आरोग्यावर चांगला परिणाम होतो आम्ही बनवलेल्या सरबते ही कोकणातच नाही तर इतर भागातही पसंत ठरलं आहे.`,
         Category: 'syrup'
     },
 
@@ -34,8 +31,7 @@ const Syrup = [
         productImage: 'https://i.postimg.cc/SNHz3H4z/syrup-Products-4.png',
         productName: 'आमरस',
         productPrice: '170',
-        productDetail: `   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident officia placeat, animi asperiores maxime accusamus. Nulla magni animi praesentium atque sed ab voluptas excepturi porro, dolorem maiores amet reprehenderit quos, ipsum laudantium eligendi nemo laboriosam quas cumque officiis quaerat quo quidem quia iure. Aliquam nam temporibus aut consequuntur doloremque!
-`,
+        productDetail: ` आमरस पूर्णतः नॅचरल कोकणातील हापूस आंब्यापासून बनवलेला आमरस यात कोणत्याही प्रकारचे कृत्रिम कलर अगर तो जास्त दिवस टिकावा यासाठी प्रिझर्वेटिव्ह नसलेला असा आमरस आरोग्यास कोणत्याही प्रकारचा अपाय करणारा नाही. केक बनवताना अशा अनेक खाद्यपदार्थ मध्ये वापरू शकता हा तुम्हाला एक लिटर अर्धा लिटर व 850 ml च्या टीम कॅन मध्ये उपलब्ध आहे.`,
         Category: 'syrup'
     },
 
