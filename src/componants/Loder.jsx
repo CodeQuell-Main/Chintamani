f0929004181d0aa5354e2c870f703435f877f862
Krishna Kadukar <krishnakadukar004@gmail.com>
2025-01-26 12:24:20 +0530

    done

--- src/componants/Loder.jsx
+++ src/componants/Loder.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Loder() {
   return (
     <div>
-      <div class="flex justify-center items-center flex-col gap-2 w-[100vw] h-[100vh] bg-[#faf7f0]  loder">
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
-        <div class="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+      <div className="flex justify-center items-center flex-col gap-2 w-[100vw] h-[100vh] bg-[#faf7f0]  loder">
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
+        <div className="size-4 bg-orange-400 rounded-2xl item shadow-2xl"></div>
       </div>
     </div>
   );
